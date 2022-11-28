60086bee56de0f4a0cb6b60e573b58a6ac102bc8
med1213 <arsenallaos646@gmail.com>
2022-11-28 14:11:23 +0700

    edit product detail

--- frontend/src/components/products/ProductDetails.js
+++ frontend/src/components/products/ProductDetails.js
@@ -43,14 +43,14 @@ const ProductDetails = () => {
             </div>
             <div className="col-12 col-lg-5 mt-5">
               <h3>{product.name}</h3>
-              <p id="product_id">Product # sklfjdk35fsdf5090</p>
+              <p id="product_id">Product # {product._id}</p>
               <hr />
               <div className="rating-outer">
                 <div className="rating-inner" />
               </div>
-              <span id="no_of_reviews">(5 Reviews)</span>
+              <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
               <hr />
-              <p id="product_price">$108.00</p>
+              <p id="product_price">${product.price}</p>
               <div className="stockCounter d-inline">
                 <span className="btn btn-danger minus">-</span>
                 <input
@@ -74,14 +74,7 @@ const ProductDetails = () => {
               </p>
               <hr />
               <h4 className="mt-2">Description:</h4>
-              <p>
-                Binge on movies and TV episodes, news, sports, music and more!
-                We insisted on 720p High Definition for this 32" LED TV,
-                bringing out more lifelike color, texture and detail. We also
-                partnered with Roku to bring you the best possible content with
-                thousands of channels to choose from, conveniently presented
-                through your own custom home screen.
-              </p>
+              <p>{product.description}</p>
               <hr />
               <p id="product_seller mb-3">
                 Sold by: <strong>Amazon</strong>
